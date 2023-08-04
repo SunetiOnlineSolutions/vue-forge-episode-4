8c61eaea6f964b4ef3d544bda41c3d8b356255b5
Dusan Petkovic <dusan.petkovic95@hotmail.com>
2023-08-04 11:38:59 +0200

    Code.

--- components/AppAlerts.vue
+++ components/AppAlerts.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-const { alerts, success, error, warning, info, dismiss } = useAlerts();
+// const { alerts, success, error, warning, info, dismiss } = useAlerts();
+const { alerts, dismiss } = useAlerts();
 </script>
 <template>
-  <div
-    class="fixed bottom-0 z-10 w-full p-2 right-2 sm:p-0 sm:w-auto sm:bottom-5"
-  >
-    <TransitionGroup name="list" tag="ul">
-      <div
-        v-for="alert in alerts"
-        :key="alert.id"
-        class="mb-4 border-t-[1px] shadow-sm alert !pr-0"
-        :class="`border-t-${alert.type}`"
-      >
+  <div class="fixed bottom-0 z-10 w-full p-2 right-2 sm:p-0 sm:w-auto sm:bottom-5" >
+    <TransitionGroup name="list" tag="div">
+      <AlertIcon :type="alert.type" />
+      <div v-for="alert in alerts" :key="alert.id" class="mb-4 border-t-[1px] shadow-sm alert !pr-0" :class="`border-t-${alert.type}`" >
         <div class="flex justify-between sm:w-80">
           <div class="flex items-center">
             <div class="mx-4">
@@ -19,36 +14,26 @@ const { alerts, success, error, warning, info, dismiss } = useAlerts();
               <div class="">{{ alert.message }}</div>
             </div>
           </div>
-          <button
-            v-if="alert.dismissiable"
-            @click="dismiss(alert)"
-            class="p-1 transition rounded hover:bg-white"
-          >
-            x
-          </button>
+          <button v-if="alert.dismissiable"  @click="dismiss(alert)" class="btn btn-ghost p-1 transition rounded hover:bg-white" ></button>
         </div>
       </div>
     </TransitionGroup>
   </div>
 </template>
-
 <style scoped>
 .list-move, /* apply transition to moving elements */
 .list-enter-active,
 .list-leave-active {
   transition: all 0.6s ease;
 }
-
 .list-enter-from {
   opacity: 0;
   transform: translateX(60px);
 }
-
 .list-leave-to {
   opacity: 0;
   transform: translateX(60px) translateY(-100%);
 }
-
 /* ensure leaving items are taken out of layout flow so that moving
    animations can be calculated correctly. */
 .list-leave-active {
--- composables/useAlerts.ts
+++ composables/useAlerts.ts
@@ -1,60 +1,54 @@
 import { nanoid } from "nanoid";
 interface AlertOptions {
-  type?: "success" | "error" | "info" | "warning";
-  title?: string;
-  dismissiable?: boolean;
-  timeout?: number;
+    type?: "success" | "error" | "info" | "warning";
+    title?: string;
+    dismissiable?: boolean;
+    timeout?: number;
 }
 interface Alert extends AlertOptions {
-  message: string;
-  id: string;
+    message: string;
+    id: string;
 }
-
+const TIMEOUT = 5000;
 export const useAlerts = () => {
-  const alerts = useState<Alert[]>("appAlerts", () => []);
-  function alert(message: string, options: AlertOptions) {
-    const id = nanoid();
-    const defaults: Partial<Alert> = {
-      type: "info",
-      dismissiable: true,
-      timeout: 5000,
-    };
-    alerts.value.push({ id, ...defaults, message, ...options });
-
-    let timeout =
-      options.timeout === undefined ? defaults.timeout : options.timeout;
-    if (timeout) {
-      setTimeout(() => dismiss(id), timeout);
+    const alerts = ref<Alert[]>("appAlerts", () => []);
+    function dismiss(idOrAlert: string | Alert) {
+        const id = typeof idOrAlert === "string" ? idOrAlert : idOrAlert.id;
+        alerts.value = alerts.value.filter((alert) => alert.id !== id);
     }
-  }
-
-  function dismiss(idOrAlert: string | Alert) {
-    const id = typeof idOrAlert === "string" ? idOrAlert : idOrAlert.id;
-    alerts.value = alerts.value.filter((alert) => alert.id !== id);
-  }
-
-  function success(message: string, options: AlertOptions = {}) {
-    alert(message, { ...options, type: "success" });
-  }
-
-  function error(message: string, options: AlertOptions = {}) {
-    alert(message, { ...options, type: "error" });
-  }
-
-  function info(message: string, options: AlertOptions = {}) {
-    alert(message, { ...options, type: "info" });
-  }
-
-  function warning(message: string, options: AlertOptions = {}) {
-    alert(message, { ...options, type: "warning" });
-  }
-
-  return {
-    success,
-    info,
-    warning,
-    error,
-    alerts,
-    dismiss,
-  };
+    function addAlert(message: string, options: AlertOptions) {
+        const id = nanoid();
+        const Alert = {
+            id,
+            message,
+            type: options.type || "info",
+            dismissiable: options.dismissiable || true,
+            timeout: options.timeout || 5000,
+            title: options.title || "",
+        }
+        alerts.value.push(alert);
+        if(alert.timeout) {
+            setTimeout(() => dismiss(id), alert.timeout);
+        }
+    }
+    function success(message: string, options: AlertOptions = {}) {
+        addAlert(message, { ...options, type: "success" });
+    }
+    function error(message: string, options: AlertOptions = {}) {
+        addAlert(message, { ...options, type: "error" });
+    }
+    function info(message: string, options: AlertOptions = {}) {
+        addAlert(message, { ...options, type: "info" });
+    }
+    function warning(message: string, options: AlertOptions = {}) {
+        addAlert(message, { ...options, type: "warning" });
+    }
+    return {
+        success,
+        info,
+        warning,
+        error,
+        alerts,
+        dismiss,
+    };
 };
